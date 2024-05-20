1e278b9f8171fdac33a52ff5389b01a8a80286db
Michael Brookins <michael.brookins@rfsmart.com>
2024-05-20 17:41:07 -0400

    Images added to style.css and script.js comments added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 
+//searchBreweries
 async function searchBreweries() {
     const searchCriteria = document.getElementById('searchCriteria').value;
     const searchInput = document.getElementById('searchInput').value.trim();
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
-    
+
+//Validate that search input is not null error handling
     if (searchInput === '') {
         resultsDiv.innerHTML = '<p>Brewery Search Criteria Can Not Be Blank</p>';
         return;
-    }
+    };
+
+//try block to handle potential errors bringing back error in catch block
+//searchCriteia path is called from html searchCriteria container 
+//searchInput is the query path 
     try {
         const response = await fetch(`https://api.openbrewerydb.org/breweries?${searchCriteria}=${searchInput}`);
         const breweries = await response.json();
+
+    //No results return error
         if (breweries.length === 0) {
             resultsDiv.innerHTML = '<p>No breweries found. Please try again.</p>';
             return;
         }
+
+//Unordered list to bring back all breweries. Inside the ul is a list that pulls back and orders details for each brewery and appends to the ul
         const ul = document.createElement('ul');
         breweries.forEach(brewery => {
             const li = document.createElement('li');
@@ -30,11 +40,15 @@ async function searchBreweries() {
             ul.appendChild(li);
         });
         resultsDiv.appendChild(ul);
+
+//catch block is trigged if there is an error during fetching 
     } catch (error) {
         console.error('Error fetching data:', error);
         resultsDiv.innerHTML = '<p>Failed to fetch breweries. Please try again later.</p>';
     }
 }
+
+//Listener to know when Search Breweries button is clicked console logging success or failue
 document.getElementById('searchButton').addEventListener('click', async () => {
     try {
         await searchBreweries();
